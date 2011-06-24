e7eee86741e0b4cecd92136dcbcf24b2c04a7689
secretrobotron <secretrobotron@gmail.com>
2011-06-24 12:00:20 -0400

    layout for subsystems

---
+++ src/paladin.js
@@ -0,0 +1,12 @@
+(function (window, document) {
+
+  var Paladin = window.Paladin = function () {
+
+    if (this === window) {
+      throw new Error('Must instantiate Paladin with "new".');
+    } //if
+
+  };
+
+})(window, document);
+
---
+++ src/paladin.subsystem.js
@@ -0,0 +1,6 @@
+(function (window, document, Paladin) {
+
+  var Subsystem = Paladin.prototype.Subsystem = function ( name, options ) {
+  };
+
+})(window, document, Paladin);
