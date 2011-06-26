5dac7ac3b3e99fc9e35e3a50edb25a1b1cb6f796
Alan Kligman <alan.kligman@gmail.com>
2011-06-25 20:45:58 -0400

    Entities can listen to and ignore events. Sending and handling javascript events does not work yet.

--- src/paladin.js
+++ src/paladin.js
@@ -1,9 +1,18 @@
 (function (window, document) {
 
 function Paladin() {
+    
+    var that = this;
+    
     if( this === window ) {
         throw new Error( 'Must instantiate Paladin with "new".' );
     }
+    
+    this.onMouseDown = function () { return function (ev)
+    {
+        alert( '!' );
+    }; }();
+    
 };
 
 window.Paladin = Paladin;
@@ -73,7 +82,75 @@ function Tasker() {
 
 }; //Tasker
 
+function Messenger() {
+    
+    var callbacks = {},
+        that = this;
+    
+    this.listen = function( options ) {
+        if( !callbacks.hasOwnProperty( options.event ) )
+            callbacks[options.event] = {};
+        callbacks[options.event][options.entity.getId()] = {
+            callback: options.callback,
+            parameters: options.parameters,
+            persistent: options.persistent
+        };
+    };
+    
+    this.ignore = function( options ) {
+        if( callbacks.hasOwnProperty( options.event ) &&
+                callbacks[options.event].hasOwnProperty( options.entity.getId() ) )
+            delete callbacks[options.event][options.entity.getId()];
+        if( 0 == Object.keys( callbacks[options.event] ).length )
+            delete callbacks[options.event];
+    };
+    
+    this.ignoreAll = function( options ) {
+        
+    };
+    
+    this.send = function( options ) {        
+    };
+    
+};
+
+// FIXME(alan.kligman@gmail.com): This is a hack.
+var nextEntityId = 0;
+
+function Entity() {
+    
+    var id = nextEntityId ++,
+        that = this;
+    
+    this.getId = function() {
+        return id;
+    };
+    
+    this.listen = function( options ) {
+        Paladin.messenger.listen( {
+            entity: that,
+            event: options.event,
+            callback: options.callback,
+            parameters: options.parameters || undefined,
+            persistent: options.persistent || true
+        } );
+    };
+    
+    this.ignore = function( options ) {
+        Paladin.messenger.ignore( {
+            entity: that,
+            event: options.event
+        } );
+    };
+    
+};
+
+// Attach core instances to Paladin.
 Paladin.tasker = new Tasker();
+Paladin.messenger = new Messenger();
+
+// Attach prototypes to Paladin.
+Paladin.Entity = Entity;
 
 /*
 Tasker.prototype.add = function( name, callback ) {
