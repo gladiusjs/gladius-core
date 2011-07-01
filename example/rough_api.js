991b44755f0e26a4fddae31aa30428c12316a6f6
Alan Kligman <alan.kligman@gmail.com>
2011-07-01 16:02:34 -0400

    Minor update to rough API.

--- example/rough_api.js
+++ example/rough_api.js
@@ -12,13 +12,13 @@ function Game() {
     var mesh = new Paladin.graphics.Mesh();     // graphics.Mesh is not a component
 
     var obj1 = new Paladin.Entity();
-    obj1.addComponent( 'spatial', new Paladin.components.Spatial() );
-    obj1.addComponent( 'model', new Paladin.components.Model( mesh ) );
+    obj1.addComponent( 'spatial', new Paladin.component.Spatial() );
+    obj1.addComponent( 'model', new Paladin.component.Model( mesh ) );  // component.Model should be a SceneObject
     obj1.setParent( render );
 
     var obj2 = new Paladin.Entity();
-    obj2.addComponent( 'spatial', new Paladin.components.Spatial() );
-    obj2.addComponent( 'model', new Paladin.components.Model( mesh ) );
+    obj2.addComponent( 'spatial', new Paladin.component.Spatial() );
+    obj2.addComponent( 'model', new Paladin.component.Model( mesh ) );
     obj2.setParent( render );
 
     Paladin.tasker.add( {
