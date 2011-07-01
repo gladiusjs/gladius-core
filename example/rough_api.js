0a7f3dc1034d31ad6c545832cdb0676d83a35bba
Alan Kligman <alan.kligman@gmail.com>
2011-07-01 16:00:55 -0400

    Rough API example. Does not run.

---
+++ example/rough_api.js
@@ -0,0 +1,39 @@
+function Game() {
+
+    var paladin = this.paladin = new Paladin();
+
+    var render = new Paladin.Scene();
+
+    var camera = new Paladin.Entity();
+    camera.addComponent( new Paladin.components.Spatial() );
+    camera.addComponent( new Paladin.graphics.Camera() );
+    camera.setParent( render );
+
+    var mesh = new Paladin.graphics.Mesh();     // graphics.Mesh is not a component
+
+    var obj1 = new Paladin.Entity();
+    obj1.addComponent( 'spatial', new Paladin.components.Spatial() );
+    obj1.addComponent( 'model', new Paladin.components.Model( mesh ) );
+    obj1.setParent( render );
+
+    var obj2 = new Paladin.Entity();
+    obj2.addComponent( 'spatial', new Paladin.components.Spatial() );
+    obj2.addComponent( 'model', new Paladin.components.Model( mesh ) );
+    obj2.setParent( render );
+
+    Paladin.tasker.add( {
+        callback: function( task ) {
+            obj1.getComponent( 'spatial' ).setRotation();            
+            return task.CONT;
+        },
+        name: 'rotateObject1'
+    } );
+    Paladin.tasker.add( {
+        callback: function( task ) {
+            obj2.getComponent( 'spatial' ).setPosition();
+            return task.CONT;
+        },
+        name: 'moveObject2'
+    } );
+
+};
