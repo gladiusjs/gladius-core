4974758a515edd1fc9138793a1917a27deacff90
Hasan Kamal-Al-Deen <hasank1987@gmail.com>
2011-09-30 16:05:23 -0400

    Added get function and partial definition of node

--- src/core/Configurator.js
+++ src/core/Configurator.js
@@ -15,13 +15,99 @@ define( function ( require ) {
 
     var Configurator = function( engine, defaultConfiguration ) {
 
+        var self = this;
+        
         defaultConfiguration = defaultConfiguration || {};
+        
+        // Define tree object
+        var ConfNode = function( name, parent ) {
+            
+            var self = this;
+            
+            self.name = name;
+            self.parent = parent;
+            self.children = {};
+            self.listeners = {};
+            
+            // Node's configuration value
+            var _value = '';
+            Object.defineProperty( self, 'value', {
+                get: function() {
+                    return _value;
+                },
+                
+                set: function( value ) {
+                    if ( _value !==  value ) {  // Additionally check if incoming value is string?
+                    
+                        // Meaningful change, notify parent
+                        if ( parent ) {
+                            parent.notify( self.name );
+                        }
+                        
+                        _value = value;
+                    }
+                }
+            });
+            
+            // Add a listener to this node's list of listeners
+            self.addListener = function( clientId, listener ) {
+                self.listeners[clientId] = listener;
+            };
+            
+            // Remove an existing listener
+            self.removeListener = function( clientId ) {
+                if ( self.listeners[clientId] != undefined ) {
+                    delete self.listeners[clientId];
+                }
+            }
+        };
+        
+        // Client front-end functions
+        
+        // Get a value based on a given path
+        self.get = function( path ) {
+            var rv = '';
+            
+            if ( path.length > 0 ) {
+                if ( path.length == 1 && path.charAt( 0 ) == '/' ) {
+                    rv = self.node.value;
+                } else {
+                    
+                    // Parse path and traverse the node tree
+                    var pathElems = path.split('/');
+                    var curNode = self.node;
+                    var successful = true;
+                    for ( var i = 0; successful && i < pathElems.length; ++ i ) {
+                        var curElem = pathElems[i];
+                        
+                        if ( curElem != '' ) {
+                            
+                            // Look for name in children of current node
+                            var nextNode = curNode.children[curElem];
+                            if ( nextNode != undefined ) {
+                                curNode = nextNode;
+                            } else {
+                                // Path leads nowhere, leave
+                                successful = false;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if ( successful ) {
+                        rv = curNode.value;
+                    }
+                }
+            }
+            
+            return rv;
+        };
 
         // Create internal tree
-        // Create client front end
+        // Associate client front end with tree root
+        
         // Return client
     };
 
     return Configurator;
-
 });
