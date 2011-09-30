8541e6c9bb7eab8a9922e729a70d0cf5e80d25ba
Hasan Kamal-Al-Deen <hasank1987@gmail.com>
2011-09-30 13:50:06 -0400

    Beginning work on configurator development.

---
+++ src/core/Configurator.js
@@ -0,0 +1,27 @@
+/*jshint white: false, strict: false, plusplus: false, onevar: false,
+  nomen: false */
+/*global define: false, console: false, window: false, setTimeout: false */
+
+
+define( function ( require ) {
+    
+    /* Configurator
+     *
+     * Loads and stores configuration data. Allows external code to listen for
+     * changes in configuration subtrees.
+     *
+     * Initially written by Hasan (northWind) Kamal-Al-Deen
+     */
+
+    var Configurator = function( engine, defaultConfiguration ) {
+
+        defaultConfiguration = defaultConfiguration || {};
+
+        // Create internal tree
+        // Create client front end
+        // Return client
+    };
+
+    return Configurator;
+
+});
