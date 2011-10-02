9e412d61ebc767458d74aa8634f856a6ae9a6e24
Alan Kligman <alan.kligman@gmail.com>
2011-10-02 11:05:36 -0400

    Working on Transform.

--- src/core/component/Transform.js
+++ src/core/component/Transform.js
@@ -12,7 +12,7 @@ define( function ( require ) {
 
             option = options || {};
 
-            var _position = new math.Vector3( math.vector3.zero );  // x, y z        
+            var _position = math.Vector3( math.vector3.zero );        
             Object.defineProperty( this, 'position', {
                 get: function() {
                     return _position;
@@ -23,34 +23,18 @@ define( function ( require ) {
                 }
             });
 
-            var _rotation = new math.Quaternion( math.quaternion.identity );    // x, y, z, w
+            var _rotation = math.Vector3( math.vector3.zero );
             Object.defineProperty( this, 'rotation', {
                 get: function() {
                     return _rotation;
                 },
                 set: function( value ) {
-                    if( 4 === value.length ) {  // quaternion
-                        if( !math.quaternion.equal( value, _rotation ) )
-                            _rotation = value;
-                    } else if( 3 === value.length ) {   // euler angles
-                        var c1 = Math.cos((Math.PI / 180.0) * value[0] / 2.0);
-                        var s1 = Math.sin((Math.PI / 180.0) * value[0] / 2.0);
-                        var c2 = Math.cos((Math.PI / 180.0) * value[1] / 2.0);
-                        var s2 = Math.sin((Math.PI / 180.0) * value[1] / 2.0);
-                        var c3 = Math.cos((Math.PI / 180.0) * value[2] / 2.0);
-                        var s3 = Math.sin((Math.PI / 180.0) * value[2] / 2.0);
-                        var c1c2 = c1 * c2;
-                        var s1s2 = s1 * s2;
-
-                        _rotation[0] = c1c2 * s3 + s1s2 * c3;
-                        _rotation[1] = s1 * c2 * c3 + c1 * s2 * s3;
-                        _rotation[2] = c1 * s2 * c3 - s1 * c2 * s3;
-                        _rotation[3] = c1c2 * c3 - s1s2 * s3;                    
-                    }
+                    if( !math.vector3.equal( value, _rotation ) )
+                        _rotation = value;
                 }
             });
 
-            var _scale = new math.Vector3( math.vector3.one );  // x, y, z
+            var _scale = new math.Vector3( math.vector3.one );
             Object.defineProperty( this, 'scale', {
                 get: function() {
                     return _scale;
