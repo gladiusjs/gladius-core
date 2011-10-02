216808a519f01f09cec0a54bc6daca2549707ef8
Alan Kligman <alan.kligman@gmail.com>
2011-10-02 11:29:25 -0400

    Transform implementation should work now, once the math library is updated.

--- src/core/component/Transform.js
+++ src/core/component/Transform.js
@@ -15,48 +15,72 @@ define( function ( require ) {
             var _position = math.Vector3( math.vector3.zero );        
             Object.defineProperty( this, 'position', {
                 get: function() {
-                    return _position;
+                    return math.Vector3( _position );
                 },
                 set: function( value ) {
-                    if( !math.vector3.equal( value, _position ) )
+                    if( !math.vector3.equal( value, _position ) ) {                       
                         _position = value;
+                        _valid = false;
+                    }
                 }
             });
 
             var _rotation = math.Vector3( math.vector3.zero );
             Object.defineProperty( this, 'rotation', {
                 get: function() {
-                    return _rotation;
+                    return math.Vector3( _rotation );
                 },
                 set: function( value ) {
-                    if( !math.vector3.equal( value, _rotation ) )
+                    if( !math.vector3.equal( value, _rotation ) ) {
                         _rotation = value;
+                        _valid = false;
+                    }
                 }
             });
 
             var _scale = new math.Vector3( math.vector3.one );
             Object.defineProperty( this, 'scale', {
                 get: function() {
-                    return _scale;
+                    return math.Vector3( _scale );
                 },
                 set: function( value ) {
-                    if( !math.vector3.equal( value, _scale ) )
+                    if( !math.vector3.equal( value, _scale ) ) {
                         _scale = value;
+                        _valid = false;
+                    }
                 }
             });
-
-            var _local = null;
-            Object.defineProperty( this, 'local', {
+            
+            // True if the cached transform is valid, false otherwise.
+            var _valid = true;
+            
+            var _absolute = math.transform.fixed( _position, _rotation, _scale );
+            var _relative = math.matrix4.inverse( _absolute );
+            
+            Object.defineProperty( this, 'absolute', {
                 get: function() {
+                    if( !_valid ) {
+                        _absolute = math.transform.fixed( _position, _rotation, _scale );
+                        _relative = math.matrix4.inverse( _absolute );
+                        _valid = true;
+                    }
+
+                    return _absolute;
                 }
             });
-
-            var _world = null;
-            Object.defineProperty( this, 'world', {
+            
+            Object.defineProperty( this, 'relative', {
                 get: function() {
+                    if( !_valid ) {
+                        _absolute = math.transform.fixed( _position, _rotation, _scale );
+                        _relative = math.matrix4.inverse( _absolute );
+                        _valid = true;
+                    }
 
+                    return _relative;                    
                 }
             });
+
         };
         Transform.prototype = new engine.Component({
             type: 'Transform'
