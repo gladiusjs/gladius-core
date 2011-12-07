119db2e08da5e41d1be58518070f744a15b2c871
Alan Kligman <alan.kligman@gmail.com>
2011-12-07 10:12:00 -0500

    Updated simple example to have two cubes, one parented to the other.

--- example/simple/simple.js
+++ example/simple/simple.js
@@ -1,11 +1,29 @@
-var game = function( engine ) {
-    // Create a new task to log the frame count
-    var t = new engine.scheduler.Task({
-        callback: function() {
-            console.log( engine.scheduler.frame );
-        }
-    });
+var printd = function( div, str ) {
+	document.getElementById( div ).innerHTML += str;
+};
 
+var game = function( engine ) {
+    var math = engine.math;
+    var space = new engine.core.Space();
+
+    var cubes = [];
+
+    cubes.push( new space.Entity({
+        name: 'cube0'
+    }) );
+    cubes[0].add( new engine.core.component.Transform({
+        position: math.Vector3( 1, 2, 3 )
+    }) );
+
+    cubes.push( new space.Entity({
+        name: 'cube1'
+    }) );
+    cubes[1].add( new engine.core.component.Transform({
+        position: math.Vector3( 1, 1, 1 )
+    }) );
+
+    cubes[1].parent = cubes[0];
+    
     // Start the engine!
     engine.run();
 };
