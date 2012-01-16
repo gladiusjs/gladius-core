b7069c288efbe82887b77568ee4b204113d9237f
Alan Kligman <alan.kligman@gmail.com>
2012-01-16 12:47:23 -0500

    Updated cube example so that it renders now. But there's a bug when attaching a child entity.

--- example/cube/cube.js
+++ example/cube/cube.js
@@ -1,94 +1,112 @@
-var printd = function( div, str ) {
-    document.getElementById( div ).innerHTML += str;
-};
+document.addEventListener( "DOMContentLoaded", function( e ){
 
-var game = function( engine ) {
-    var math = engine.math;
-    var resources = {}; 
+    var printd = function( div, str ) {
+        document.getElementById( div ).innerHTML += str;
+    };
 
-    var game = function() {
+    var canvas = document.getElementById( "test-canvas" );    
+    var resources = {};
 
-        // Make a new space for our entities
-        var space = new engine.core.Space();
+    var game = function( engine ) {
+        var math = engine.math;
 
-        // Make some entities and arrange them
-        var cubes = [];
+        var game = function() {
 
-        cubes.push( new space.Entity({
-            name: 'cube0',
-            components: [
+            // Make a new space for our entities
+            var space = new engine.core.Space();
+
+            // Make some entities and arrange them
+            var cubes = [];
+
+            canvas = engine.graphics.target.element;
+
+            cubes.push( new space.Entity({
+                name: 'cube0',
+                components: [
 new engine.core.component.Transform({
     position: math.Vector3( 0, 0, 0 )
 }),
 new engine.graphics.component.Model({
-    mesh: resources.mesh
+    mesh: resources.mesh,
+    material: resources.material
 })
 ]
-        }) );
+            }) );
 
-        cubes.push( new space.Entity({
-            name: 'cube1',
-            components: [
+            cubes.push( new space.Entity({
+                name: 'cube1',
+                components: [
 new engine.core.component.Transform({
     position: math.Vector3( 1, 1, 1 ),
     scale: math.Vector3( 0.1, 0.1, 0.1 )
 }),
 new engine.graphics.component.Model({
-    mesh: resources.mesh
+    mesh: resources.mesh,
+    material: resources.material
 })
 ]
-        }) );
+            }) ); 
 
-        var camera = new space.Entity({
-            name: 'camera',
-            components: [
+            var camera = new space.Entity({
+                name: 'camera',
+                components: [
 new engine.core.component.Transform({
-    position: math.Vector3( 0, 0, 0 )
+    position: math.Vector3( 10, 0, 0 )
 }),
 new engine.graphics.component.Camera({
     active: true,
+    width: canvas.width,
+    height: canvas.height,
     fov: 60
 })
 ]
+            });
+            camera.find( 'Camera' ).target = cubes[0].find( 'Transform' ).position;
+
+            // cubes[1].parent = cubes[0];
+
+            // Start the engine!
+            engine.run();
+
+        };
+
+        var expectedResources = 2;
+        var registerResource = function( name, instance ) {
+            resources[name] = instance;
+            if( Object.keys( resources ).length === expectedResources ) {
+                game();
+            }
+        };
+
+        engine.graphics.resource.Mesh({
+            script: engine.graphics.script.mesh.cube,
+            onsuccess: function( instance ) {
+                registerResource( 'mesh', instance );
+            }
+        });
+        engine.graphics.resource.Material({
+            script: engine.graphics.script.material.sample,
+            onsuccess: function( instance ) {
+                registerResource( 'material', instance );
+            }
         });
 
-        cubes[1].parent = cubes[0];
-
-        // Start the engine!
-        engine.run();
-
     };
 
-    var expectedResources = 2;
-    var registerResource = function( name, instance ) {
-        resources[name] = instance;
-        if( Object.keys( resources ).length === expectedResources ) {
-            game();
-        }
-    };
 
-    engine.graphics.resource.Mesh({
-        script: engine.graphics.script.mesh.cube,
-        onsuccess: function( instance ) {
-            registerResource( 'mesh', instance );
-        }
-    });
-    engine.graphics.resource.Material({
-        script: engine.graphics.script.material.sample,
-        onsuccess: function( instance ) {
-            registerResource( 'material', instance );
-        }
-    });
+    gladius.create(
+            {
+                debug: true,
+                services: {
+                    graphics: {
+                        src: 'graphics/service',
+                        options: {
+                            canvas: canvas
+                        }
+                    }
+                }
+            },
+            game
+    );
 
-};
-
-gladius.create(
-        {
-            debug: true,
-            services: {
-                behavior: 'logic/action-list/service',
-                graphics: 'graphics/service'
-            }
-        },
-        game
-);
+});
