aa3ad65690cfc0c3caeddace3d9c0cc428af10f6
Alan Kligman <alan.kligman@gmail.com>
2012-01-24 16:50:17 -0500

    Example and a bit of implementation for input.

--- src/gladius-src.js
+++ src/gladius-src.js
@@ -147,7 +147,7 @@ define( function ( require ) {
             if ( this.options.setup ) {
                 this.options.setup( this );
             }
-
+            
             // Let caller know the engine instance is ready.
             if (callback) {
                 callback(this);
@@ -162,7 +162,7 @@ define( function ( require ) {
             run: function () {
                 if ( this.options.run ) {
                     this.options.run( this );
-                }                
+                }            
                 this.scheduler.resume();                
             },
     
