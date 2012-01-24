6d83d2fb17e6d6ce06a67b987239eb288693db5b
Alan Kligman <alan.kligman@gmail.com>
2012-01-24 12:20:51 -0500

    Added a stub for controller.

---
+++ src/input/component/controller.js
@@ -0,0 +1,22 @@
+/*jshint white: false, strict: false, plusplus: false, onevar: false,
+  nomen: false */
+/*global define: false, console: false, window: false, setTimeout: false */
+
+define( function ( require ) {
+
+    return function( engine ) {
+
+        var math = engine.math;
+        var Component = require( 'base/component' );
+
+        return Component({
+            type: 'Controller'
+        },
+        function( options ) {
+            
+        });
+       
+    
+    };
+    
+});
