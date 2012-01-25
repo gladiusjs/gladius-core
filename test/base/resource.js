0d4a1666af746aeb939d22a56b44425b8d76af3c
Alan Kligman <alan.kligman@gmail.com>
2012-01-25 17:54:06 -0500

    Fixed a unit test, and added some new ones for base/resource.

--- test/base/resource.js
+++ test/base/resource.js
@@ -26,8 +26,7 @@
 
     asyncTest( 'construct a new resource type and an instance of it', 
            function() {
-             expect(1);
-             
+             expect(1);             
 
              var Text = engine.base.Resource({
                type: 'Text',
@@ -50,42 +49,92 @@
                }
              });
            });
-
-    // TD: constructing new resource type without type param throws exception
-        
-    // TD: test that a type with a default loader invokes that loader
-    
-    // TD: test that default loader can be overridden with a closure when
-    // creating an procedurally loaded instance of a given type
-    
+   
     // Some day we can do even better by using mocks and ensuring that any HTTP
     // status code < 200 or > 299 fails.
     asyncTest( 'ensure onfailure gets called on loading non-existent file',
                 function () {
                   
-        expect(1);             
-    
-        var options = {
-            url : "no-such-url-exists",
-
-            onsuccess : function itemOnSuccess(item) {
-                ok( false, "non-existent file load shouldn't call onsuccess" );
-                start();
-            },
-
-            onfailure : function itemOnFailure(error) {
-                ok( true, "non-existent file load should call onfailure");
-
-                // TD should check format of error once we decide it
-
-                start();                
+        expect(1);
+        
+        var Text = engine.base.Resource({
+            type: 'Text',
+            construct: function constructText( data ) {
+              this.value = data;
             }
-        };
-
-        // TD: this construction mode is wrong.  need to fix.
-        var resource = new engine.base.Resource()(options);
+          });
+        
+        Text({
+            url: "no-such-url-exists",
+            onsuccess: function onTextSuccess( text ) {
+                ok( false, "non-existent file load shouldn't call onsuccess" );
+                start();            },
+            onfailure: function onTextFailure( error ) {
+                ok( true, "non-existent file load should call onfailure");
+                start();
+            }
+          });
+    
      });
+    
+    test( 'custom resource without type throws an exception',
+      function () {
+              
+        expect(1);
+    
+        raises( function() {
+            var Text = engine.base.Resource({
+                construct: function constructText( data ) {
+                    this.value = data;
+                }
+            });
+        }, function( exception ) {
+            return exception.message == "missing type parameter";
+        }, 'base resource throws exception for missing type parameter');
 
+      });
+    
+    asyncTest( 'construct a new resource type and an instance of it', 
+            function() {
+              expect(3);
+              
+              var testUrl = 'test-url';
+
+              var Text = engine.base.Resource({
+                type: 'Text',
+                load: function loadText( url ) {
+                    equal( url, testUrl, 'url is passed into load function' );
+                    return url;
+                },
+                construct: function constructText( data ) {
+                  equal( data, testUrl, 'data returned by load is correct' );
+                  this.value = data;
+                }
+              });
+                
+              Text({
+                url: testUrl,
+                onsuccess: function onTextSuccess( text ) {
+                  deepEqual( text ,
+                     { value: testUrl },
+                     "text object is constructed from loaded data");
+                  start();
+                },
+                onfailure: function onTextFailure( error ) {
+                  ok(false, "onTextFailure improperly called with " + error);
+                  start();
+                }
+              });
+            });
+    
+    
+    // TD: test that default loader can be overridden with a closure when
+    // creating an procedurally loaded instance of a given type
+
+    // TD: test that onfailure is invoked if load fails or returned undefined
+    
+    // TD: test that onfailure is invoked if construct fails
+   
 }());
 
      
