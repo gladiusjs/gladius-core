ec2c156a8a4d87248ef39d7dff618abafa028801
Dan Mosedale <dmose@meer.net>
2012-01-25 12:57:45 -0800

    Added test of API to load a typed resource

--- test/base/resource.js
+++ test/base/resource.js
@@ -1,6 +1,6 @@
 /*jshint white: false, strict: false, plusplus: false, onevar: false,
   nomen: false */
-/*global module: false, gladius: false, stop: false, start: false,
+/*global module: false, gladius: false, stop: false, start: false, test: false,
   expect: false, asyncTest: false, ok: false, deepEqual: false, equal: false */
 
 (function() {
@@ -24,6 +24,33 @@
         }
     });
 
+    asyncTest( 'construct a new resource type and an instance of it', 
+           function() {
+             expect(1);
+             
+
+             var Text = engine.base.Resource({
+               type: 'Text',
+               construct: function constructText( data ) {
+                 this.value = data;
+               }
+             });
+               
+             Text({
+               url: "data:text/plain,Hello%20World",
+               onsuccess: function onTextSuccess( text ) {
+                 deepEqual( text ,
+                    { value: "Hello World" },
+                    "text object constructed from data URL");
+                 start();
+               },
+               onfailure: function onTextFailure( error ) {
+                 ok(false, "onTextFailure improperly called with " + error);
+                 start();
+               }
+             });
+           });
+    
     // Some day we can do even better by using mocks and ensuring that any HTTP
     // status code < 200 or > 299 fails.
     asyncTest( 'ensure onfailure gets called on loading non-existent file',
@@ -54,17 +81,12 @@
         // this object model needs simplifying.
         var resource = new engine.base.Resource()(options);
      });
+
+}());
+
      
      // TD: test data URLs, and commutativity/normalization of params:
      //
      // "data:gladius/script;core.script.test?x=1&y=2"
      // "data:gladius/script;core.script.test?y=2&x=1"
   
-     // TD: test that result not populated before oncomplete is called and 
-     // result returned on async completion
-     
-     // TD test loading of a resource when passing a constructor for the
-     // for processing the returned JSON:
-     //
-     // var resource = new engine.base.Resource(someConstructor)(options)
-}());
