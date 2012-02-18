52e08b5be4d8f143d77281bd222d50218a8001e1
Dan Mosedale <dmose@meer.net>
2012-02-17 18:46:24 -0800

    Fix render testing, move it to the top-level test page, and delete the graphics-specific test page

--- test/graphics/service.js
+++ test/graphics/service.js
@@ -1,117 +1,123 @@
 /*jshint white: false, strict: false, plusplus: false, onevar: false,
-  nomen: false */
+ nomen: false */
 /*global gladius: false, document: false, window: false, module: false, start,
-  test: false, expect: false, ok: false, notEqual: false, stop, QUnit: false,
-  asyncTest: false, equal: false */
+ test: false, expect: false, ok: false, notEqual: false, stop, QUnit: false,
+ asyncTest: false, equal: false */
 
-(function() {
+( function() {
 
-    var engine = null;
+  var engine = null;
 
-    module( 'graphics/service', {
-        setup: function () {
-            stop();
+  module('graphics/service', {
+    setup : function() {
+      stop();
 
-            var canvas = document.createElement( "canvas" );
-            canvas.width = 300;
-            canvas.height = 300;
-            document.getElementById( "canvas-container" ).appendChild( canvas );
-
-            gladius.create({
-                  debug: true,
-                  services: {
-                      graphics: {
-                          src: 'graphics/service',
-                          options: {
-                              canvas: canvas
-                          }
-                      }
-                  }
-              }, function( instance ) {       
-                  engine = instance;
-                  start();
-            });
-        },
-
-        teardown: function () {
-            engine = null;
+      var canvas = document.getElementById("test-canvas");
+      gladius.create({
+        debug : true,
+        services : {
+          graphics : {
+            src : 'graphics/service',
+            options : {
+              canvas : canvas
+            }
+          }
         }
+      }, function(instance) {
+        engine = instance;
+        start();
+      });
+    },
+    teardown : function() {
+      engine = null;
+    }
+  });
+
+  function createCubeMesh() {
+    var point = 0.5;
+
+    var meshData = {
+            points:   [
+                [ point, -point,  point],
+                [ point,  point,  point],
+                [-point,  point,  point],
+                [-point, -point,  point],
+                [ point, -point, -point],
+                [ point,  point, -point],
+                [-point,  point, -point],
+                [-point, -point, -point]
+                      ],
+            faces:    [
+                [0, 1, 2, 3],
+                [7, 6, 5, 4],
+                [4, 5, 1, 0],
+                [5, 6, 2, 1],
+                [6, 7, 3, 2],
+                [7, 4, 0, 3]
+                      ],
+            uv:       [
+                [ [0, 1], [1, 1], [1, 0], [0, 0] ],
+                [ [0, 1], [1, 1], [1, 0], [0, 0] ],
+                [ [0, 1], [1, 1], [1, 0], [0, 0] ],
+                [ [0, 1], [1, 1], [1, 0], [0, 0] ],
+                [ [0, 1], [1, 1], [1, 0], [0, 0] ],
+                [ [0, 1], [1, 1], [1, 0], [0, 0] ]
+                      ],
+            uvmapper: {
+                projectionMode: "cubic",
+                scale: [1, 1, 1]
+                      }
+        };
+ 
+    return new engine.graphics.resource.Mesh(meshData);
+  }
+
+
+  test('Construction', function() {
+    expect(1);
+    ok(engine.graphics, 'graphics subsystem exists');
+  });
+
+
+  asyncTest('Test render', function() {
+    expect(1);
+    var scene = new engine.core.Space(), 
+        cameraEntity = new scene.Entity(), 
+        modelEntity = new scene.Entity();
+
+    var canvas = engine.graphics.target.element;
+
+    var cameraComponent = new engine.graphics.component.Camera({
+      active : true,
+      width : canvas.width,
+      height : canvas.height,
+      fov : 60
+    });
+    cameraEntity.add(new engine.core.component.Transform({
+      position : [1, 1, 1]
+    }));
+    cameraEntity.add(cameraComponent);
+    cameraComponent.target = [0.2, 0, 0];
+
+    modelEntity.add(new engine.core.component.Transform({
+      position : [0, 0, 0]
+    }));
+
+
+    var modelComponent = new engine.graphics.component.Model({
+      mesh : createCubeMesh(),
+      material : new engine.graphics.resource.Material({ color : [1, 0.2, 0] } )
     });
 
-    test( 'Construction', function() {
-        expect( 1 );
-        ok( engine.graphics, 'graphics subsystem exists' );
-    });
+    cameraEntity.add(new engine.graphics.component.Light({ intensity : 50 }));
 
-    asyncTest( 'Test render', function() {
-        expect( 1 );
-        var scene = new engine.core.Space(),
-            cameraEntity = new scene.Entity(),
-            modelEntity = new scene.Entity();
-
-        var canvas = engine.graphics.target.element;
-
-        var cameraComponent = new engine.graphics.component.Camera({
-          active: true,
-          width: canvas.width,
-          height: canvas.height,
-          fov: 60
-        });
-        cameraEntity.add( new engine.core.component.Transform({
-            position: [1, 1, 1]
-        }));
-        cameraEntity.add( cameraComponent );
-        cameraComponent.target = [0.2, 0, 0];
-
-        modelEntity.add( new engine.core.component.Transform({
-            position: [0, 0, 0]
-        }));
-
-        var resources = {};
-        function onResourceSuccess(){
-            if( resources.mesh && resources.material && resources.light ){
-                var lightComponent = new engine.graphics.component.Light( resources.light );
-                var modelComponent = new engine.graphics.component.Model({
-                    mesh: resources.mesh,
-                    material: resources.material,
-                    onready: function( instance ) {
-                        modelEntity.add( instance );
-                        engine.graphics.render();
-                        engine.run();
-                        setTimeout( function() {
-                            engine.run();
-                            ok( engine.graphics.renderedFrames > 2, "Graphics task is rendering" );
-                            start();
-                        }, 500 );
-                    }
-                });
-                cameraEntity.add( lightComponent );
-            } //if
-        } //onResourceSuccess
-
-        engine.graphics.resource.Mesh({
-            script: engine.graphics.script.mesh.cube,
-            onsuccess: function( mesh ) {
-                resources.mesh = mesh;
-                onResourceSuccess();
-            }
-        });
-
-        engine.graphics.resource.Material({
-            script: engine.graphics.script.material.sample,
-            onsuccess: function( material ) {
-                resources.material = material;
-                onResourceSuccess();
-            }
-        });
-
-        engine.graphics.resource.Light({
-            script: engine.graphics.script.light.sample,
-            onsuccess: function( light ) {
-                resources.light = light;
-                onResourceSuccess();
-            }
-        });
-
-    });
+    modelEntity.add(modelComponent);
+    engine.graphics.render();
+    engine.run();
+    setTimeout(function() {
+      engine.run();
+      ok(engine.graphics.renderedFrames > 2, "Graphics task is rendering");
+      start();
+    }, 500);
+  });
 }());
