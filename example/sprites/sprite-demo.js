1492d4d8ca0925f1ac534bfcf6d852e9ad0a01f3
Dan Mosedale <dmose@meer.net>
2012-02-23 17:32:13 -0800

    Split the BitwallModel code out of the sprites demo using require; reformat; and cleanup.

--- example/sprites/sprite-demo.js
+++ example/sprites/sprite-demo.js
@@ -1,38 +1,33 @@
 /*global Sprite, viking, console */
+
+var engine, CubicVR;
+
 document.addEventListener("DOMContentLoaded", function(e) {
 
+  // TODOs
+  // * hoist bitwall model into other file so we can iterate while andor uses
+  // * instead of rotation, use walk-back and walk-front with transforms & scaling
+  // * sort out the loading story
+  // * make the animation speed sane
 
-// TODOs
-// * hoist bitwall model into other file so we can iterate while andor uses
-// * instead of rotation, use walk-back and walk-front with transforms & scaling
-// * sort out the loading story
-// * make the animation speed sane
-
-  var BitwallModel = require('bitwall-model');
-  
-  var printd = function(div, str) {
-    document.getElementById(div).innerHTML = str + '<p>';
-  };
-  var cleard = function(div) {
-    document.getElementById(div).innerHTML = '';
-  };
   var canvas = document.getElementById("test-canvas");
 
-  var game = function(engine) {
+  var game = function(engineInstance) {
+    engine = engineInstance;
+    CubicVR = engine.graphics.target.context;
     var math = engine.math;
-
-    var CubicVR = engine.graphics.target.context;
-
+       
     var thugAction = 'walk-front';
 
-     var run = function() {
+    var run = function() {
 
       // Make a new space for our entities
       var space = new engine.core.Space();
 
-      // Make some entities and arrange them
       canvas = engine.graphics.target.element;
 
+      // because we've already asynchronously loaded our sprite '
+      var BitwallModel = require('../example/sprites/bitwall-model');
       var bitwall = new space.Entity({
         name : 'cube0',
         components : [new engine.core.component.Transform({
@@ -65,37 +60,39 @@ document.addEventListener("DOMContentLoaded", function(e) {
       // is gonna want some optimization too.
       var animationTime = 10;
       var animationTimer = 0;
-        
+
       var task = new engine.scheduler.Task({
         schedule : {
           phase : engine.scheduler.phases.UPDATE
         },
         callback : function() {
           var delta = engine.scheduler.simulationTime.delta / 1000;
-          bitwall.find('Transform').rotation = 
-            math.matrix4.add([bitwall.find('Transform').rotation,
-                             [0, math.TAU * delta * 0.1, 0]]);
-          
-          if (!animationTimer) {
+          bitwall.find('Transform').rotation = math.matrix4.add([bitwall.find('Transform').rotation, [0, math.TAU * delta * 0.1, 0]]);
+
+          if(!animationTimer) {
             // XXX update animation
             bitwall.find('Model').updateAction(thugAction);
 
             // reset the timer
-            animationTimer = animationTime;            
+            animationTimer = animationTime;
 
-          } else {
-            --animationTimer;
+          } else {--animationTimer;
           }
-      }});
+        }
+      });
 
       // Start the engine!
       engine.run();
 
     };
+    function loadSprite() {
+      viking.loadSprite('./thug1.sprite', {
+        callback : run
+      });
+    }
 
-    viking.loadSprite('./thug1.sprite', {
-      callback : run
-    });
+    // XXX shouldn't require path contortions
+    require(['../example/sprites/bitwall-model'], loadSprite);
 
   };
 
