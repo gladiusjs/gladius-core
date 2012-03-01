4793c55639e92460c80d8a03828f84981526aba1
Alan Kligman <alan.kligman@gmail.com>
2012-03-01 12:39:17 -0500

    1. Single event dispatch no longer requires the target to be passed in a list. 2. Example code refactoring. 3. Collision example behavior is changed.

--- example/collision-cubes/collision-cubes.js
+++ example/collision-cubes/collision-cubes.js
@@ -16,14 +16,116 @@ document.addEventListener( "DOMContentLoaded", function( e ){
         var math = engine.math;
 
         var CubicVR = engine.graphics.target.context;
+        
+        var MotionService = engine.base.Service({
+            type: 'Motion',
+            schedule: {
+                update: {
+                    phase: engine.scheduler.phases.UPDATE
+                }
+            },
+            time: engine.scheduler.simulationTime
+        },
+        function( options ) {
+            var that = this;
+            var service = this;
+            options = options || {};
+            
+            this.update = function() {
+                var updateEvent = new engine.core.Event({
+                    type: 'Update',
+                    queue: false,
+                    data: {
+                        delta: that.time.delta
+                    }
+                });
+                for( var componentType in that.components ) {
+                    for( var entityId in that.components[componentType] ) {
+                        var component = that.components[componentType][entityId];
+                        while( component.handleQueuedEvent() ) {}
+                        updateEvent.dispatch( component );
+                    }
+                }
+            };
+            
+            var Planar = engine.base.Component({
+                type: 'Motion',
+                depends: ['Transform']
+            },
+            function( options ) {
+                var that = this;
+                var _directions = {
+                    UP: options.forward || math.Vector3( 0, 1, 0 ),
+                    LEFT: options.right || math.Vector3( -1, 0, 0 ),
+                    RIGHT: options.reverse || math.Vector3( 1, 0, 0 ),
+                    DOWN: options.left || math.Vector3( 0, -1, 0 )
+                };
+                var _move = null;
+                var _lastPosition = null;
+                this.velocity = null;
+                
+                this.onMoveStart = function( event ) {
+                    _move = event.data.direction;
+                };
+                
+                this.onMoveStop = function( event ) {
+                    _move = null;
+                };
+                               
+                this.onUpdate = function( event ) {
+                    var transform = this.owner.find( 'Transform' );
 
-        var Collision2Service = engine.base.Service({
+                    if( _move ) {
+                        var direction = math.vector3.multiply( _directions[_move], event.data.delta / 1000 );
+                        transform.position = math.vector3.add( transform.position, direction );
+                    }
+                    
+                    that.velocity = math.vector2.subtract( transform.position, _lastPosition );
+                    _lastPosition = transform.position;
+                };
+                
+                // Boilerplate component registration; Lets our service know that we exist and want to do things
+                this.onComponentOwnerChanged = function( e ){
+                    if( e.data.previous === null && this.owner !== null ) {
+                        service.registerComponent( this.owner.id, this );
+                    }
+
+                    if( this.owner === null && e.data.previous !== null ) {
+                        service.unregisterComponent( e.data.previous.id, this );
+                    }
+                    _lastPosition = this.owner.find( 'Transform' ).position;
+                };
+
+                this.onEntityManagerChanged = function( e ) {
+                    if( e.data.previous === null && e.data.current !== null && this.owner !== null ) {
+                        service.registerComponent( this.owner.id, this );
+                    }
+
+                    if( e.data.previous !== null && e.data.current === null && this.owner !== null ) {
+                        service.unregisterComponent( this.owner.id, this );
+                    }
+                };
+            });
+            
+            var _components = {
+                Planar: Planar
+            };
+            Object.defineProperty( this, 'component', {
+                get: function() {
+                    return _components;
+                }
+            });
+        });
+        engine.motion = new MotionService();
+
+        var CollisionService = engine.base.Service({
             type: 'Physics',
             schedule: {
                 update: {
                     phase: engine.scheduler.phases.UPDATE
                 }
             },
+            depends: [ 'Motion' ],
             time: engine.scheduler.simulationTime
         },
         function( options ) {
@@ -122,7 +224,7 @@ document.addEventListener( "DOMContentLoaded", function( e ){
                                     data: {
                                         entity: component2.owner
                                     }
-                                }).dispatch( [component1.owner] );
+                                }).dispatch( component1.owner );
                             }
 
                         }
@@ -131,9 +233,7 @@ document.addEventListener( "DOMContentLoaded", function( e ){
             };
 
             var _components = {
-
                     AABB: AABB
-
             };
 
             Object.defineProperty( this, 'component', {
@@ -142,8 +242,7 @@ document.addEventListener( "DOMContentLoaded", function( e ){
                 }
             });
         });
-
-        var collision2Service = new Collision2Service();
+        engine.collision = new CollisionService();
 
         var PlayerComponent = engine.base.Component({
             type: 'Player',
@@ -157,35 +256,12 @@ document.addEventListener( "DOMContentLoaded", function( e ){
             // This is a hack so that this component will have its message
             // queue processed
             var service = engine.logic; 
-
-            var moveStates = {
-                    LEFT: -1,
-                    IDLE: 0,
-                    RIGHT: 1
-            };
-            var moveState = moveStates.IDLE;
-            var speed = options.speed || 10;
-
-            this.onStartMove = function( event ) {
-                moveState = moveStates[event.data.direction];                             
-            };
-
-            this.onStopMove = function( event ) {
-                moveState = moveStates.IDLE;
-            };
-            
+           
             this.onCollision = function( event ) {
                 console.log( that.owner.id, '->', event.data.entity.id );
             };
 
             this.onUpdate = function( event ) {
-                var transform = this.owner.find( 'Transform' );
-                var delta = service.time.delta;
-
-                transform.position = math.vector3.add(
-                        transform.position,
-                        [moveState * delta/1000 * speed, 0, 0]
-                );
             };
 
             // Boilerplate component registration; Lets our service know that we exist and want to do things
@@ -218,11 +294,28 @@ document.addEventListener( "DOMContentLoaded", function( e ){
 
             canvas = engine.graphics.target.element;
 
-            var cube0 = new space.Entity({
-                name: 'cube0',
+            // Make an obstacle that will collide with the player
+            var obstacle = new space.Entity({
+                name: 'obstacle',
+                components: [
+                             new engine.core.component.Transform(),
+                             new engine.graphics.component.Model({
+                                 mesh: resources.mesh,
+                                 material: resources.material
+                             }),
+                             new engine.collision.component.AABB({
+                                 halfWidth: 1,
+                                 halfHeight: 1
+                             })
+                             ]
+            });
+
+            // Make a user-controllable object that can collide with the obstacle
+            var player = new space.Entity({
+                name: 'player',
                 components: [
                              new engine.core.component.Transform({
-                                 position: math.Vector3( -2, 0, 0 ),
+                                 position: math.Vector3( 3, 3, 0 ),
                                  rotation: math.Vector3( 0, 0, 0 )
                              }),
                              new engine.graphics.component.Model({
@@ -235,85 +328,57 @@ document.addEventListener( "DOMContentLoaded", function( e ){
                                          // If we have an owner, dispatch a game event for it to enjoy
                                          var rotate;
                                          switch( e.data.code ) {
-                                         case 'A':
+                                         case 'W':
                                              new engine.core.Event({
-                                                 type: e.data.state === 'down' ? 'StartMove' : 'StopMove',
+                                                 type: e.data.state === 'down' ? 'MoveStart' : 'MoveStop',
                                                          data: {
-                                                             direction: 'LEFT'
+                                                             direction: 'UP'
                                                          }
-                                             }).dispatch( [this.owner] );
+                                             }).dispatch( this.owner );
+                                             break;
+                                         case 'A': 
+                                             new engine.core.Event({
+                                                 type: e.data.state === 'down' ? 'MoveStart' : 'MoveStop',
+                                                         data: {
+                                                             direction: 'LEFT'                                                             
+                                                         }
+                                             }).dispatch( this.owner );
+                                             break;
+                                         case 'S': 
+                                             new engine.core.Event({
+                                                 type: e.data.state === 'down' ? 'MoveStart' : 'MoveStop',
+                                                         data: {
+                                                             direction: 'DOWN'                                                             
+                                                         }
+                                             }).dispatch( this.owner );
                                              break;
                                          case 'D': 
                                              new engine.core.Event({
-                                                 type: e.data.state === 'down' ? 'StartMove' : 'StopMove',
-                                                         data: {
-                                                             direction: 'RIGHT'
-                                                         }
-                                             }).dispatch( [this.owner] );
-                                             break;
-                                         }
-                                     }
-                                 }
-                             }),
-                             new PlayerComponent(),
-                             new collision2Service.component.AABB({
-                                 halfWidth: 1,
-                                 halfHeight: 1
-                             })
-                             ]
-            });
-
-            var cube1 = new space.Entity({
-                name: 'cube1',
-                components: [
-                             new engine.core.component.Transform({
-                                 position: math.Vector3( 2, 0, 0 ),
-                                 rotation: math.Vector3( 0, 0, 0 )
-                             }),
-                             new engine.graphics.component.Model({
-                                 mesh: resources.mesh,
-                                 material: resources.material
-                             }),
-                             new engine.input.component.Controller({
-                                 onKey: function(e) {
-                                     if( this.owner ) {
-                                         // If we have an owner, dispatch a game event for it to enjoy
-                                         var rotate;
-                                         switch( e.data.code ) {
-                                         case 'J':
-                                             new engine.core.Event({
-                                                 type: e.data.state === 'down' ? 'StartMove' : 'StopMove',
-                                                         data: {
-                                                             direction: 'LEFT'
-                                                         }
-                                             }).dispatch( [this.owner] );
-                                             break;
-                                         case 'L': 
-                                             new engine.core.Event({
-                                                 type: e.data.state === 'down' ? 'StartMove' : 'StopMove',
+                                                 type: e.data.state === 'down' ? 'MoveStart' : 'MoveStop',
                                                          data: {
                                                              direction: 'RIGHT'                                                             
                                                          }
-                                             }).dispatch( [this.owner] );
+                                             }).dispatch( this.owner );
                                              break;
+
                                          }
                                      }
                                  }
                              }),
-                             new PlayerComponent(),
-                             new collision2Service.component.AABB({
+                             new engine.motion.component.Planar(),
+                             new engine.collision.component.AABB({
                                  halfWidth: 1,
                                  halfHeight: 1
-                             })
+                             }),
+                             new PlayerComponent()
                              ]
             });
-
 
             var camera = new space.Entity({
                 name: 'camera',
                 components: [
                              new engine.core.component.Transform({
-                                 position: math.Vector3( 0, 0, 10 )
+                                 position: math.Vector3( 0, 1, 10 )
                              }),
                              new engine.graphics.component.Camera({
                                  active: true,
@@ -324,17 +389,7 @@ document.addEventListener( "DOMContentLoaded", function( e ){
                              new engine.graphics.component.Light({ intensity: 50 })
                              ]
             });
-            camera.find( 'Camera' ).target = math.Vector3( 0, 0, 0 );
-
-            var task = new engine.scheduler.Task({
-                schedule: {
-                    phase: engine.scheduler.phases.UPDATE
-                },
-                callback: function() {
-                    var delta = engine.scheduler.simulationTime.delta/1000;
-
-                }
-            });
+            camera.find( 'Camera' ).target = obstacle.find( 'Transform' ).position;
 
             // Start the engine!
             engine.run();
