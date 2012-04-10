2110c70a627ecbc5bc8d3602252e65708cdd6c54
Alan Kligman <alan.kligman@gmail.com>
2012-04-10 12:04:53 -0400

    Hid some internal variables.

--- wip-src/clock.js
+++ wip-src/clock.js
@@ -3,40 +3,40 @@ define( function( require ) {
   var Clock = function() {
     this.time = 0;
     this.delta = 0;
-    this.timeScale = 1.0;
-    this.idealFrameInterval = 1.0/30.0;
+    this._timeScale = 1.0;
+    this._idealFrameInterval = 1.0/30.0;
     
     this.C_STARTED = 0;
     this.C_PAUSED = 1;
    
-    this.clockState = this.C_STARTED;
+    this._clockState = this.C_STARTED;
   };
   
   function suspend() {
-    this.clockState = this.C_PAUSED;
+    this._clockState = this.C_PAUSED;
   };
   
   function resume() {
-    this.clockState = this.C_STARTED;
+    this._clockState = this.C_STARTED;
   };
   
   function update( delta ) {
-    if( this.C_PAUSED !== this.clockState ) {
-      this.delta = delta * this.timeScale;
+    if( this.C_PAUSED !== this._clockState ) {
+      this.delta = delta * this._timeScale;
       this.time += this.delta;
     }
   };
   
   function step( count ) {
     count = undefined === count ? 1 : count;
-    if( this.C_PAUSED === this.clockState ) {
-      this.delta = count * this.idealFrameInterval * this.timeScale;
+    if( this.C_PAUSED === this._clockState ) {
+      this.delta = count * this._idealFrameInterval * this._timeScale;
       this.time += this.delta;
     }
   };
   
   function isPaused() {
-    return this.clockState === this.C_PAUSED;
+    return this._clockState === this.C_PAUSED;
   };
   
   function reset() {
@@ -44,13 +44,23 @@ define( function( require ) {
     this.delta = 0;
   };
   
+  function setTimeScale( scale ) {
+    this._timeScale = scale;
+  };
+  
+  function setIdealFrameInterval( interval ) {
+    this._idealFrameInterval = interval;
+  }
+  
   Clock.prototype = {
      suspend: suspend,
      resume: resume,
      update: update,
      isPaused: isPaused,
      step: step,
-     reset: reset
+     reset: reset,
+     setTimeScale: setTimeScale,
+     setIdealFrameInterval: setIdealFrameInterval
   };
   
   return Clock;
--- wip-src/clock.test.js
+++ wip-src/clock.test.js
@@ -13,9 +13,9 @@ define(
           
           var clock = new Clock();
           
-          equal( clock.idealFrameInterval, 1.0/30.0, 
+          equal( clock._idealFrameInterval, 1.0/30.0, 
               "default ideal frame interval is correct" );
-          equal( clock.timeScale, 1.0, "default time scale is correct" );
+          equal( clock._timeScale, 1.0, "default time scale is correct" );
           equal( clock.time, 0.0, "initial time is correct" );
           equal( clock.delta, 0.0, "initial delta is correct" );
           ok( !clock.isPaused(), "clock is not paused" );
@@ -59,8 +59,8 @@ define(
           equal( clock.delta, 0.0, "delta is unchanged after step" );
           clock.suspend();
           clock.step( 2 );
-          equal( clock.time, 2 * clock.idealFrameInterval, "time is correct" );
-          equal( clock.delta, 2 * clock.idealFrameInterval, "delta is correct" );
+          equal( clock.time, 2 * clock._idealFrameInterval, "time is correct" );
+          equal( clock.delta, 2 * clock._idealFrameInterval, "delta is correct" );
         });
         
         test( "default step", function() {
@@ -70,8 +70,8 @@ define(
           
           clock.suspend();
           clock.step();
-          equal( clock.time, clock.idealFrameInterval, "time is correct" );
-          equal( clock.delta, clock.idealFrameInterval, "delta is correct" );
+          equal( clock.time, clock._idealFrameInterval, "time is correct" );
+          equal( clock.delta, clock._idealFrameInterval, "delta is correct" );
         });
         
         test( "time scale update", function() {
@@ -79,7 +79,7 @@ define(
           
           var clock = new Clock();
           
-          clock.timeScale = 10;
+          clock.setTimeScale( 10 );
           clock.update( 1 );
           equal( clock.time, 10.0, "time is correct" );
           equal( clock.delta, 10.0, "delta is correct" );
@@ -90,12 +90,12 @@ define(
           
           var clock = new Clock();
           
-          clock.timeScale = 10;
+          clock.setTimeScale( 10 );
           clock.suspend();
           clock.step();
-          equal( clock.time, 10.0 * clock.idealFrameInterval, 
+          equal( clock.time, 10.0 * clock._idealFrameInterval, 
               "time is correct" );
-          equal( clock.delta, 10.0 * clock.idealFrameInterval, 
+          equal( clock.delta, 10.0 * clock._idealFrameInterval, 
               "delta is correct" );
         });
         
@@ -104,7 +104,7 @@ define(
           
           var clock = new Clock();
           
-          clock.idealFrameInterval = 1/60;
+          clock.setIdealFrameInterval( 1/60 );
           
           clock.suspend();
           clock.step();
