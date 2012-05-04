02a73fe3d1c29c9595e73d31a51393da2fe1f67b
Alan Kligman <alan.kligman@gmail.com>
2012-05-04 15:15:16 -0400

    Lower-cased new file. Fixed script invocation to not use run().

--- wip-src/core/loaders/procedural.js
+++ wip-src/core/loaders/procedural.js
@@ -2,7 +2,7 @@ define(function(require) {
 
   var get = require('core/get');
   var Script = require('core/resources/script');
-  var getURLParams = require('common/get-URL-Params');
+  var getURLParams = require('common/get-url-params');
   return function(url, onsuccess, onfailure) {
 
     var scriptLocation = url.split( "?" )[0];
@@ -12,7 +12,7 @@ define(function(require) {
       type : Script,
       onsuccess : function(instance) {
         try {
-          var data = instance.run( scriptOptions );
+          var data = instance( scriptOptions );
           onsuccess( data ) ;
         } catch ( e ) {
           onfailure( e );
