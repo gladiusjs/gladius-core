2d899c280c2358aed7b4f94d68216e7a8ac06889
Alan Kligman <alan.kligman@gmail.com>
2012-05-08 14:40:10 -0400

    Added stub for light definition.

---
+++ src/extensions/cubicvr/src/resources/light-definition.js
@@ -0,0 +1,13 @@
+if ( typeof define !== "function" ) {
+  var define = require( "amdefine" )( module );
+}
+
+define( function ( require ) {
+  
+  var LightDefinition = function( data ) {
+    
+  };
+  
+  return LightDefinition;
+  
+});
