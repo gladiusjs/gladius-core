093bb26cf4ed38c232d6e488d36da88e22e4be9d
Alan Kligman <alan.kligman@gmail.com>
2012-05-15 13:18:42 -0400

    Cleaned up example a bit.

--- examples/simple-dist/simple.js
+++ examples/simple-dist/simple.js
@@ -3,7 +3,5 @@ document.addEventListener( "DOMContentLoaded", function( e ){
   var engine = new Gladius();
   console.log( "engine loaded" );
   engine.registerExtension( Gladius["gladius-cubicvr"] );
-  engine._scheduler.update();
-  console.log( engine._scheduler._schedule );
 
 });
