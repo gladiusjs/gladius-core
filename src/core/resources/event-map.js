afa5bde0ffd6bd9a0969cb51ce28581f38aedc57
Alan Kligman <alan.kligman@gmail.com>
2012-06-02 20:24:09 -0400

    First round of commits for logic service.

---
+++ src/core/resources/event-map.js
@@ -0,0 +1,33 @@
+define( function( require ) {
+
+  var get = require( "core/get" );
+  var Script = require( "core/resources/script" );
+
+  var EventMap = function( data ) {
+    data = data || {};
+    var map = {};
+
+    var getRequests = [];
+    var eventNames = Object.keys( data );
+
+    for( var eventName in eventNames ) {
+      getRequests.push({
+        type: Script,
+        url: data[eventName],
+        onsuccess: function( script ) {
+          map[eventName] = script;
+        },
+        onfailure: function( error ) {
+          console.log( "error loading script: " + data[eventName] );
+          throw error;
+        }
+      });
+    }
+    get( getRequests );
+
+    return map;
+  };
+
+  return EventMap;
+
+});
