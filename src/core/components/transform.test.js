cce9b50f871ca5e4ed583c3ef1d625575a39b1cc
David Perit <dperit@gmail.com>
2012-07-17 12:33:40 -0400

    Finished unit tests for old functionality under new math API for transform

--- src/core/components/transform.test.js
+++ src/core/components/transform.test.js
@@ -1,26 +1,14 @@
 define(
-    [ "core/components/transform", 
+    [ "core/components/transform",
+      "core/entity",
       "_math" ],
-    function( Transform, math ) {
+    function( Transform, Entity, math ) {
       return function() {
 
         module( "Transform", {
           setup: function() {},
           teardown: function() {}
         });
-
-        /* Tests to write:
-         * X construct a transform, no paramaters
-         * X construct a transform, position only
-         * X construct a transform, rotation only
-         * X construct a transform, scale only
-         * - set position, rotation, scale using assignment, check local & world matrix
-             transform.position = [1, 2, 3]
-         * - set position, rotation, scale components, check local & world matrix
-             transform.position.x = 1
-           - create a compound transform (position, rotation, scale), check local & world matrix
-           - create a transform tree, check local & world matrix for all transforms
-         */
         
         test( "construct a transform, no parameters", function() {
           expect( 6 );
@@ -65,7 +53,7 @@ define(
           ok( transform.scale.equal( [2, 3, 4] ), "scale is correct" );
         });
 
-        test( "set position, assignment", function() {
+        test( "set position using assignment", function() {
           expect( 4 );
 
           var transform = new Transform();
@@ -78,7 +66,137 @@ define(
           ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
           ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
         });
-       
+
+        test( "set rotation using assignment", function(){
+          expect( 4 );
+
+          var transform = new Transform();
+          transform.rotation = [1, 2, 3];
+
+          ok( transform.rotation instanceof math.Vector3, "rotation is a Vector3" );
+          ok( transform.rotation.equal( [1, 2, 3] ), "rotation is correct" );
+          var expectedLocalMatrix = expectedWorldMatrix = new math.transform.rotation( [1, 2, 3] );
+
+          ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
+          ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
+        });
+
+        test( "set scale using assignment", function(){
+          expect( 4 );
+
+          var transform = new Transform();
+          transform.scale = [1, 2, 3];
+
+          ok( transform.scale instanceof math.Vector3, "position is a Vector3" );
+          ok( transform.scale.equal( [1, 2, 3] ), "position is correct" );
+          var expectedLocalMatrix = expectedWorldMatrix = new math.transform.scale( [1, 2, 3] );
+
+          ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
+          ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
+        });
+
+        test( "set position using component assignment", function() {
+          expect( 4 );
+
+          var transform = new Transform();
+          transform.position.x = 1;
+          transform.position.y = 2;
+          transform.position.z = 3;
+
+          ok( transform.position instanceof math.Vector3, "position is a Vector3" );
+          ok( transform.position.equal( [1, 2, 3] ), "position is correct" );
+          var expectedLocalMatrix = expectedWorldMatrix = new math.transform.position( [1, 2, 3] );
+
+          ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
+          ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
+        });
+
+        test( "set rotation using component assignment", function() {
+          expect( 4 );
+
+          var transform = new Transform();
+          transform.rotation.x = 1;
+          transform.rotation.y = 2;
+          transform.rotation.z = 3;
+
+          ok( transform.rotation instanceof math.Vector3, "rotation is a Vector3" );
+          ok( transform.rotation.equal( [1, 2, 3] ), "rotation is correct" );
+          var expectedLocalMatrix = expectedWorldMatrix = new math.transform.rotation( [1, 2, 3] );
+
+          ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
+          ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
+        });
+
+        test( "set scale using component assignment", function() {
+          expect( 4 );
+
+          var transform = new Transform();
+          transform.scale.x = 1;
+          transform.scale.y = 2;
+          transform.scale.z = 3;
+
+          ok( transform.scale instanceof math.Vector3, "scale is a Vector3" );
+          ok( transform.scale.equal( [1, 2, 3] ), "scale is correct" );
+          var expectedLocalMatrix = expectedWorldMatrix = new math.transform.scale( [1, 2, 3] );
+
+          ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
+          ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
+        });
+
+        test( "create a compound transform (position, rotation, scale)", function() {
+          expect( 2 );
+
+          var position = [1,2,3];
+          var rotation = [4,5,6];
+          var scale = [7,8,9];
+          var transform = new Transform( position, rotation, scale );
+
+          var expectedMatrix = math.transform.fixed(position, rotation, scale);
+          ok(math.matrix4.equal( transform.localMatrix(), expectedMatrix), "local matrix is correct");
+          ok(math.matrix4.equal( transform.worldMatrix(), expectedMatrix), "world matrix is correct");
+        });
+
+        test( "create a transform tree", function() {
+          expect( 6 );
+
+          var position1 = [1,2,3];
+          var rotation1 = [4,5,6];
+          var scale1 = [7,8,9];
+          var position2 = [10,11,12];
+          var rotation2 = [13,14,15];
+          var scale2 = [16,17,18];
+          var position3 = [19,20,21];
+          var rotation3 = [22,23,24];
+          var scale3 = [25,26,27];
+          var transform1 = new Transform( position1, rotation1, scale1 );
+          var transform2 = new Transform( position2, rotation2, scale2 );
+          var transform3 = new Transform( position3, rotation3, scale3 );
+
+          var entity1 = new Entity("entity1", [transform1]);
+          var entity2 = new Entity("entity2", [transform2], [], entity1);
+          var entity3 = new Entity("entity3", [transform3], [], entity2);
+
+          var expected1 = math.transform.fixed( position1, rotation1, scale1 );
+          var expected2 = math.transform.fixed( position2, rotation2, scale2 );
+          var expected3 = math.transform.fixed( position3, rotation3, scale3 );
+
+          var worldMatrix2 = math.matrix4.multiply(expected2, transform1.worldMatrix());
+          var worldMatrix3 = math.matrix4.multiply(expected3, transform2.worldMatrix());
+
+          ok(math.matrix4.equal(transform1.worldMatrix(), expected1),
+            "first world matrix is correct");
+          ok(math.matrix4.equal(transform2.worldMatrix(), worldMatrix2),
+            "second world matrix is correct");
+          ok(math.matrix4.equal(transform3.worldMatrix(), worldMatrix3),
+            "third world matrix is correct");
+
+          ok(math.matrix4.equal(transform1.localMatrix(), expected1),
+            "first local matrix is correct");
+          ok(math.matrix4.equal(transform2.localMatrix(), expected2),
+            "second local matrix is correct");
+          ok(math.matrix4.equal(transform3.localMatrix(), expected3),
+            "third local matrix is correct");
+        });
       };
     }
 );
