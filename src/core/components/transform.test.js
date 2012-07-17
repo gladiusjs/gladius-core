bb7d4b2d37cca2da3a163dc926cc7959d3df8340
Alan Kligman <alan.kligman@gmail.com>
2012-07-17 11:41:44 -0400

    Updating unit tests.

--- src/core/components/transform.test.js
+++ src/core/components/transform.test.js
@@ -8,78 +8,77 @@ define(
           setup: function() {},
           teardown: function() {}
         });
+
+        /* Tests to write:
+         * X construct a transform, no paramaters
+         * X construct a transform, position only
+         * X construct a transform, rotation only
+         * X construct a transform, scale only
+         * - set position, rotation, scale using assignment, check local & world matrix
+             transform.position = [1, 2, 3]
+         * - set position, rotation, scale components, check local & world matrix
+             transform.position.x = 1
+           - create a compound transform (position, rotation, scale), check local & world matrix
+           - create a transform tree, check local & world matrix for all transforms
+         */
         
-        test( "contruct a new transform", function() {
-          expect( 5 );
+        test( "construct a transform, no parameters", function() {
+          expect( 6 );
 
           var transform = new Transform();
-          ok( transform instanceof Transform, "transform is created" );
 
-          ok( math.vector3.equal( transform.position, math.vector3.zero ), 
-            "default position is correct" );
-          ok( math.vector3.equal( transform.rotation, math.vector3.zero ),
-            "default rotation is correct" );
-          ok( math.vector3.equal( transform.scale, math.vector3.one ),
-            "default scale is correct" );
-          ok( math.matrix4.equal( transform.absolute(), math.matrix4.identity ),
-            "default absolute matrix is correct" );
+          ok( transform.position instanceof math.Vector3, "position is a Vector3" );
+          ok( transform.rotation instanceof math.Vector3, "rotation is a Vector3" );
+          ok( transform.scale instanceof math.Vector3, "scale is a Vector3" );
+          ok( transform.position.equal( math.vector3.zero ), "position is zero" );
+          ok( transform.rotation.equal( math.vector3.zero ), "rotation is zero" );
+          ok( transform.scale.equal( math.vector3.one ), "scale is one" );
         });
 
-        test( "set position", function() {
-          expect( 2 );
+        test( "construct a transform, position only", function() {
+          expect( 3 );
 
-          var transform = new Transform( new math.Vector3( 1, 2, 3 ) );
+          var transform = new Transform( [1, 2, 3] );
 
-          ok( math.vector3.equal( transform.position, 
-            new math.Vector3( 1, 2, 3 ) ), 
-            "position is correct" );
-          transform.setPosition( [4, 5, 6] );
-          ok( math.vector3.equal( transform.position, 
-            new math.Vector3( 4, 5, 6 ) ),
-            "position is correct" );
+          ok( transform.position.equal( [1, 2, 3] ), "position is correct" );
+          ok( transform.rotation.equal( math.vector3.zero ), "rotation is zero" );
+          ok( transform.scale.equal( math.vector3.one ), "scale is one" );
         });
 
-        test( "set rotation", function() {
-          expect( 2 );
+        test( "construct a transform, rotation only", function() {
+          expect( 3 );
 
-          var transform = new Transform( undefined, new math.Vector3( 1, 2, 3 ) );
+          var transform = new Transform( null, [1, 2, 3] );
 
-          ok( math.vector3.equal( transform.rotation, 
-            new math.Vector3( 1, 2, 3 ) ), 
-            "rotation is correct" );
-          transform.setRotation( [4, 5, 6] );
-          ok( math.vector3.equal( transform.rotation, 
-            new math.Vector3( 4, 5, 6 ) ),
-            "rotation is correct" );
+          ok( transform.position.equal( math.vector3.zero ), "position is zero" );
+          ok( transform.rotation.equal( [1, 2, 3] ), "rotation is correct" );
+          ok( transform.scale.equal( math.vector3.one ), "scale is one" );
         });
 
-        test( "set scale", function() {
-          expect( 2 );
+        test( "construct a transform, scale only", function() {
+          expect( 3 );
 
-          var transform = new Transform( undefined, undefined, 
-            new math.Vector3( 1, 2, 3 ) );
+          var transform = new Transform( null, null, [2, 3 ,4] );
 
-          ok( math.vector3.equal( transform.scale, 
-            new math.Vector3( 1, 2, 3 ) ), 
-            "scale is correct" );
-          transform.setScale( [4, 5, 6] );
-          ok( math.vector3.equal( transform.scale, 
-            new math.Vector3( 4, 5, 6 ) ),
-            "scale is correct" );
+          ok( transform.position.equal( math.vector3.zero ), "position is zero" );
+          ok( transform.rotation.equal( math.vector3.zero ), "rotation is correct" );
+          ok( transform.scale.equal( [2, 3, 4] ), "scale is correct" );
         });
 
-        test( "compound transform", function() {
-          expect( 1 );
+        test( "set position, assignment", function() {
+          expect( 4 );
 
-          var transform = new Transform( [1, 2, 3], [4, 5, 6], [7, 8, 9] );
+          var transform = new Transform();
+          transform.position = [1, 2, 3];
 
-          ok( math.matrix4.equal( transform.absolute(), 
-            [1.9065481424331665, -0.554817259311676, 6.712470054626465, 0, 
-             4.11338472366333, -6.6430840492248535, -1.7174100875854492, 0, 
-             7.3196282386779785, 4.96370792388916, -1.668725848197937, 0, 
-             1, 2, 3, 1]), "absolute transform is correct" );
+          ok( transform.position instanceof math.Vector3, "position is a Vector3" );
+          ok( transform.position.equal( [1, 2, 3] ), "position is correct" );
+          var expectedLocalMatrix = expectedWorldMatrix = new math.transform.position( [1, 2, 3] );
+
+          ok( math.matrix4.equal( transform.localMatrix(), expectedLocalMatrix ), "local matrix is correct" );
+          ok( math.matrix4.equal( transform.worldMatrix(), expectedWorldMatrix ), "world matrix is correct" );
         });
-        
+       
       };
     }
 );
